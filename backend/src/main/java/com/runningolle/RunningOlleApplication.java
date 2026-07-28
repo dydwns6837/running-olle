c03cde7ea983e091161438720b049a87380f4a9c
유용준 <144577722+dydwns6837@users.noreply.github.com>
2026-07-28 18:53:00 +0900

    [chore] 초기 환경 설정

---
+++ backend/src/main/java/com/runningolle/RunningOlleApplication.java
@@ -0,0 +1,11 @@
+package com.runningolle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningOlleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RunningOlleApplication.class, args);
+    }
+}

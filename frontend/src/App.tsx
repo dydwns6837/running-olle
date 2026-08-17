005e1b979e9fae32eb85dd804f7b40e62445ec30
sttarrynight <suwo5612@naver.com>
2026-08-17 13:53:39 +0900

    공통 앱 구조와 라우팅 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-
-function HomePage() {
-  return (
-    <main className="min-h-screen p-6">
-      <h1 className="text-2xl font-bold">Running Olle</h1>
-      <p className="mt-2">러닝과 여행을 연결하는 위치 기반 서비스</p>
-    </main>
-  )
-}
+import { AppLayout } from './components/layout/AppLayout'
+import { user } from './mocks/home'
+import { CommunityPage } from './pages/Community/CommunityPage'
+import { CoursesPage } from './pages/Courses/CoursesPage'
+import { HomePage } from './pages/Home/HomePage'
+import { MyPage } from './pages/MyPage/MyPage'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route element={<AppLayout leftSlot={<span>📍 {user.location}</span>} />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/courses" element={<CoursesPage />} />
+          <Route path="/community" element={<CommunityPage />} />
+          <Route path="/mypage" element={<MyPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
